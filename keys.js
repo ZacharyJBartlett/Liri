f489d6d9949d25b33a49f8825ca39077de34e340
Zachary Bartlett <Zachary Bartlett>
2017-04-24 18:19:56 -0400

    1st commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'eo1VFxuee4SUvarYZGRHqnNnK',
+    consumer_secret: '	MrlqOl1q3u6uUQ0m6s7F8sI4Es6lJmPtXp9pcgVZOcj9ndFngK',
+    access_token_key: '855061616257368064-joMPFBWd4ZczSFgzUk6Z5T8xRsHUvq4',
+    access_token_secret: 'cWrZ2RrPIV9q0DwzdDiNl9KSCdpJBwEcRQdDopOWEbOaQ',
+}
